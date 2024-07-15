d2decd838549c96f4d0a867fb9fc6954fb8c4651
bjamse <16bjorn@gmail.com>
2024-07-15 16:05:34 +0200

    progress while at work

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
 const express = require('express')
 const path = require('path')
+const { join } = require('node:path');
+const { createServer } = require('node:http');
+const { Server } = require('socket.io');
+
 const app = express()
 const port = process.env.PORT;
+const server = createServer(app);
+const io = new Server(server);
+
 
 app.get('/', (req, res) => {
-    res.sendFile('index.html');
+    res.sendFile(join(__dirname, 'index.html'));
 })
+
 
 
 app.use('/static', express.static(path.join(__dirname, 'static')))
 
 
-app.listen(port, () => {
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    console.log(socket.id);
+
+    socket.on('submit_song', (uri)=>{
+        console.log(uri);
+
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user'+socket.id+' disconnected');
+    });
+});
+
+server.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
